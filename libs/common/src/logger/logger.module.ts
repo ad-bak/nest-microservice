fb993b5fb964a0113238a802ab781570452937dd
Adnan <adbak.dev@gmail.com>
2024-04-04 23:06:38 +0200

    microservices

--- libs/common/src/logger/logger.module.ts
+++ libs/common/src/logger/logger.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule as PinoLogger } from 'nestjs-pino';
 
-@Module({})
+@Module({
+  imports: [
+    PinoLogger.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
+})
 export class LoggerModule {}
