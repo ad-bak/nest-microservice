f5b353b7ae54c5521c043e0654a31d73a6242b96
Adnan <adbak.dev@gmail.com>
2024-04-06 03:20:00 +0200

    validate

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -4,6 +4,7 @@ import { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { config } from 'dotenv';
+import { TokenPayload } from './interfaces/token-payload.interface';
 
 config();
 
@@ -15,7 +16,7 @@ export class AuthService {
   ) {}
 
   async login(user: UserDocument, response: Response) {
-    const tokenPayload = {
+    const tokenPayload: TokenPayload = {
       userId: user._id.toHexString(),
     };
 
